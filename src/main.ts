fb3b40dc7a5f8aa46d8cec324940d2c0942f11e5
Evgeny Koleda <evgeny.koleda@katemedia.eu>
2021-01-30 17:58:14 +0300

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { Model } from 'mongoose';
 
 import config from 'src/config';
 
 import { AppModule } from 'src/modules/app/app.module';
 import { LoggerService } from 'src/modules/logger/logger.service';
-import { Logger, LoggerDocument, LoggerSchema } from 'src/modules/logger/schemas/logger.schema';
-// import { LoggerModule } from 'src/modules/logger/logger.module';
 
 async function bootstrap() {
 	let app = await NestFactory.create(AppModule, {
 		logger: new LoggerService(),
+		cors: true,
 	});
 
 	app.setGlobalPrefix('api');
