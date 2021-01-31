c04b2300d69dc63e6ba8eefa59654d86242fe587
Evgeny Koleda <evgeny.koleda@katemedia.eu>
2021-01-31 22:41:21 +0300

    Generate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 
 import config from 'src/config';
@@ -6,15 +7,25 @@ import config from 'src/config';
 import { AppModule } from 'src/modules/app/app.module';
 import { LoggerService } from 'src/modules/logger/logger.service';
 
+let swaggerConfig = new DocumentBuilder()
+	.setTitle('EVSX social network')
+	.setDescription('The EVSX social network API description')
+	.setVersion('1.0')
+	.addTag('EVSX')
+	.build();
+
 async function bootstrap() {
 	let app = await NestFactory.create(AppModule, {
 		logger: new LoggerService(),
 		cors: true,
 	});
 
+	app.use(helmet());
+
 	app.setGlobalPrefix('api');
 
-	app.use(helmet());
+	let document = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('swagger', app, document);
 
 	await app.listen(config.host.port);
 }
